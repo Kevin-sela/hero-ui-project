763fec60a5c8f2d4b872a5473cf2c633a790a25b
Prince Owusu <princeowusu.272@gmail.com>
2025-03-01 14:50:22 +0000

    minor

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import React, { forwardRef } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import VercelDarkLogo from "../../assets/vercel-dark-logo.svg";
 import VercelLightLogo from "../../assets/vercel-light-logo.svg";
@@ -53,11 +54,10 @@ export const Header = forwardRef<HTMLDivElement, HeaderProps>(
                       <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                         <li className="row-span-3">
                           <NavigationMenuLink asChild>
-                            <a
-                              className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            <Link
                               href="/"
+                              className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                             >
-                              {/* <Icons.logo className="h-6 w-6" /> */}
                               <div className="mb-2 mt-4 text-lg font-medium">
                                 Designs
                               </div>
@@ -65,7 +65,7 @@ export const Header = forwardRef<HTMLDivElement, HeaderProps>(
                                 Beautifully designed components built with Radix
                                 UI and Tailwind CSS.
                               </p>
-                            </a>
+                            </Link>
                           </NavigationMenuLink>
                         </li>
                         <ListItem href="/docs" title="Introduction">
@@ -113,24 +113,6 @@ export const Header = forwardRef<HTMLDivElement, HeaderProps>(
                         >
                           Displays an indicator showing the completion progress
                           of a task, typically displayed as a progress bar.
-                        </ListItem>
-                        <ListItem
-                          href="/docs/primitives/scroll-area"
-                          title="Scroll-area"
-                        >
-                          Visually or semantically separates content.
-                        </ListItem>
-                        <ListItem href="/docs/primitives/tabs" title="Tabs">
-                          A set of layered sections of content—known as tab
-                          panels—that are displayed one at a time.
-                        </ListItem>
-                        <ListItem
-                          href="/docs/primitives/tooltip"
-                          title="Tooltip"
-                        >
-                          A popup that displays information related to an
-                          element when the element receives keyboard focus or
-                          the mouse hovers over it.
                         </ListItem>
                       </ul>
                     </NavigationMenuContent>
@@ -216,25 +198,25 @@ export const Header = forwardRef<HTMLDivElement, HeaderProps>(
 Header.displayName = "Header";
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link> & { title: string }
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
+          {...props}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
-          {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );
